ff3c2f22f7cc1db49cf2f98ed971f1832d660589
Maddie Waldie <maddiewaldie@me.com>
2022-10-19 19:13:12 -0700

    [Frontend] Created a README for the frontend section of the repository (#22)
    * Created a README for the frontend section of the repository
    
    * Added script to install the dependencies, as per PR comment

---
+++ frontend/get_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+npm install
+npm install @mui/material @emotion/react @emotion/styled
+npm install @mui/material @mui/styled-engine-sc styled-components
+npm install @fontsource/roboto
+npm install @mui/icons-material
+npm install -S react-router-dom
