20685ca79d8d0185d4db3398dc48590cc6ee8dfb
Louie S <louie@example.com>
2022-10-20 11:47:38 -0700

    Added input sanitation to update functions; made update functions less rigid

--- backend/update.php
+++ backend/update.php
@@ -5,7 +5,64 @@ include "config.php";
 // globals
 $queries = []; //list of queries to be run
 
+function update_account($json_hash) {
+	GLOBAL $db;
+	$need_update = false; //set to true if at least one of the fields is set
+	$update_query = "";
+
+	// list of fields (excluding id)
+	$fields = [
+		"name",
+		"password"
+	];
+
+	// Quit out if id is not passed
+	if(!$json_hash['id']) {
+		print "Error: Missing id to update<br>";
+		return "";
+	}
+
+	// Check if anything is passed to update
+	foreach($fields as $f) {
+		if(array_key_exists($f, $json_hash)) {
+			$need_update = true;
+			break;
+		}
+	}
+	if(!$need_update) {
+		print "Error: Missing field(s) to update<br>";
+		return;
+	}
+
+	// Build query
+	$update_query .= "UPDATE Accounts SET";
+	foreach($fields as $f) {
+		if(array_key_exists($f, $json_hash)) {
+			$json_hash[$f] = mysqli_real_escape_string($db, $json_hash[$f]);
+			$update_query .= " $f = '" . $json_hash[$f] . "',";
+		}
+	}
+	$update_query = rtrim($update_query, ","); //remove trailing comma
+	$update_query .= " WHERE id = " . $json_hash['id'];
+
+	// Push query
+	array_push($GLOBALS['queries'], $update_query);
+}
+
 function update_task($json_hash) {
+	GLOBAL $db;
+	$need_update = false; //set to true if at least one of the fields is set
+	$update_query = "";
+
+	// list of fields (excluding task_id)
+	$fields = [
+		"account_id",
+		"tasks_name",
+		"category",
+		"deadline",
+		"priority"
+	];
+
 
 	// Quit out if task id is not passed
 	if(!$json_hash['task_id']) {
@@ -13,31 +70,32 @@ function update_task($json_hash) {
 		return;
 	}
 
-	// Create query
-	array_push(
-		$GLOBALS['queries'],
-		"UPDATE Tasks SET tasks_name = '" . $json_hash['tasks_name'] . 
-		"', category = '" . $json_hash['category'] . 
-		"', deadline = '" . $json_hash['deadline'] . 
-		"', priority = "  . $json_hash['priority'] .
-		" WHERE task_id = " . $json_hash['task_id']
-	);
-}
-
-function update_account($json_hash) {
-	// Quit out if id is not passed
-	if(!$json_hash['id']) {
-		print "Error: Missing id to update<br>";
-		return "";
+	// Check if anything is passed to update
+	foreach($fields as $f) {
+		if(array_key_exists($f, $json_hash)) {
+			$need_update = true;
+			break;
+		}
+	}
+	if(!$need_update) {
+		print "Error: Missing field(s) to update<br>";
+		return;
 	}
 
-	// Create query
-	array_push(
-		$GLOBALS['queries'], 
-		"UPDATE Accounts SET name = '" . $json_hash['name'] . 
-		"', password = '" . $json_hash['password'] . 
-		"' WHERE id = " . $json_hash['id']
-	);
+	// Build query
+	$update_query .= "UPDATE Tasks SET";
+	print "$update_query<br>";
+	foreach($fields as $f) {
+		if(array_key_exists($f, $json_hash)) {
+			$json_hash[$f] = mysqli_real_escape_string($db, $json_hash[$f]);
+			$update_query .= " $f = '" . $json_hash[$f] . "',";
+		}
+	}
+	$update_query = rtrim($update_query, ","); //remove trailing comma
+	$update_query .= " WHERE task_id = " . $json_hash['task_id'];
+
+	// Push query
+	array_push($GLOBALS['queries'], $update_query);
 }
 
 // Get query based off request type
